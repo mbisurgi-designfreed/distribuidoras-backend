7b5c8e80b75f039dc3b3cf5f014d95987f0982b9
mbisurgi <maximiliano@designfreed.com>
2017-02-07 14:42:10 -0300

    Actualizacion estado hoja de ruta

---
+++ src/main/java/com/designfreed/rest/EstadoMovimientoRestController.java
@@ -0,0 +1,27 @@
+package com.designfreed.rest;
+
+import com.designfreed.domain.EstadoEnvase;
+import com.designfreed.domain.EstadoMovimiento;
+import com.designfreed.services.EstadoMovimientoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/estadoMovimiento")
+public class EstadoMovimientoRestController {
+    private EstadoMovimientoService estadoMovimientoService;
+
+    @Autowired
+    public void setEstadoMovimientoService(EstadoMovimientoService estadoMovimientoService) {
+        this.estadoMovimientoService = estadoMovimientoService;
+    }
+
+    @GetMapping("/list")
+    public List<EstadoMovimiento> findAll() {
+        return estadoMovimientoService.findAll();
+    }
+}
